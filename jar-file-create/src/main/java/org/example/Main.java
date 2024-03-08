f396f841a1e4ead75dbdf2240bd6604694d4bf79
KavishkaSasindu <sasindu0215@gmail.com>
2024-03-08 19:48:56 +0530

    initialize commit

---
+++ jar-file-create/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+import java.util.ArrayList;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+    }
+}
